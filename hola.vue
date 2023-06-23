95cf68357d871dc15cadaff495df28f2246dfe00
davidDelgado <david.eduardo.pinchi@gmail.com>
2023-06-23 15:26:03 -0500

    feat: add curso vue platzi

---
+++ hola.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex voluptatum repudiandae sit dolor libero tenetur! Aut
+            error eaque maxime, incidunt inventore, expedita neque alias accusamus quos veniam ut, praesentium earum.</p>
+        <div>{{ text }}</div>
+    </div>
+</template>
+
+
+<script>
+export default {
+    data() {
+        return {
+            text: 'Holaaa'
+        }
+    }
+}
+</script>
+
+<style>
+    p {
+        color: aqua;
+    }
+</style>
